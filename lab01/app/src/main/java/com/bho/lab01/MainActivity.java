56d91286ee5a633ee94c8ebba08a928c3944b5a6
bhkrayola <brianalbertho@gmail.com>
2024-02-01 09:12:08 -0800

    test2

--- lab01/app/src/main/java/com/bho/lab01/MainActivity.java
+++ lab01/app/src/main/java/com/bho/lab01/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 System.out.println("incrementing: "+ ++count);
                 Log.i("incrementing", ""+count);
+                //test
             }
         });
     }
