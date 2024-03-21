f8bd4d68d99f6cc97d581115675242d37f685374
bhkrayola <brianalbertho@gmail.com>
2024-03-21 10:07:38 -0400

    hello

---
+++ lab09/app/src/main/java/com/hobrian/lab09/DrawView.java
@@ -0,0 +1,20 @@
+package com.hobrian.lab09;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DrawView extends View {
+
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
